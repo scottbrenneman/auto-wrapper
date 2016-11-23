a579572ef8c3598837a38b9fb12af2bc97b1928d
scott <scottiebrenneman@gmail.com>
2016-11-23 16:34:19 -0500

    Move codegen related extensions into codegen namespace

--- AutoWrapper/AutoWrapper/Extensions.cs
+++ AutoWrapper/AutoWrapper/Extensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.CodeDom;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -30,36 +29,6 @@ namespace AutoWrapper
 			var fn = genericType.FullName;
 
 			return $"{fn.Substring(0, fn.IndexOf('`'))}<{string.Join(", ", argTypes)}>";
-		}
-	}
-
-	internal static class CodeTypeReferenceExtensions
-	{
-		public static bool IsArray(this CodeTypeReference typeReference) => typeReference.ArrayElementType != null;
-	}
-
-	internal static class CodeAttributeDeclarationCollectionExtensions
-	{
-		public static void AddExcludeFromCodeCoverage(this CodeAttributeDeclarationCollection attributes)
-		{
-			attributes.Add(new CodeAttributeDeclaration("System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage"));
-		}
-
-		public static void AddGeneratedCode(this CodeAttributeDeclarationCollection attributes)
-		{
-			var tool = "AutoWrapper";
-
-			var version = Assembly
-				.GetAssembly(typeof(CodeAttributeDeclarationCollectionExtensions))
-				.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-				.InformationalVersion;
-
-			attributes.Add(new CodeAttributeDeclaration(
-					"System.CodeDom.Compiler.GeneratedCode",
-					new CodeAttributeArgument(new CodePrimitiveExpression(tool)),
-					new CodeAttributeArgument(new CodePrimitiveExpression(version))
-				)
-			);
 		}
 	}
 }
