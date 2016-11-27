fe974c6eb52daea694f2ebed07937efb1d0a4ae9
scott <scottiebrenneman@gmail.com>
2016-11-27 16:39:34 -0500

    Updated assemblyinfo, nuspec for 1.0.0 nuget publish

--- AutoWrapper/AutoWrapper/Properties/AssemblyInfo.cs
+++ AutoWrapper/AutoWrapper/Properties/AssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-rc1")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
 
 [assembly: InternalsVisibleTo("AutoWrapper.Tests")]
